446072f78ead4ed4479adf6c39688fd9dc1c0565
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2024-10-10 11:19:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Tricolor',
+    'Quantas vezes o São Paulo foi campeão da Copa do Mundo de Clubes da FIFA?',
+    'O São Paulo foi campeão da Copa do Mundo de Clubes da FIFA três vezes: em 1992, 1993 e 2005. '
+)
+
+criaCartao(
+    'Imperador',
+    'Qual foi a polêmica envolvendo o jogador Adriano Imperador e seu afastamento do futebol profissional?',
+    'Adriano, conhecido como “Imperador”, se afastou do futebol devido a uma série de problemas pessoais, incluindo a morte de seu pai e dificuldades com o alcoolismo. Seu comportamento fora de campo, as faltas aos treinos e a forma física foram alvo de críticas constantes, gerando debates sobre o apoio psicológico e emocional a jogadores profissionais.'
+)
+
+criaCartao(
+    '7x1',
+    'Por que a derrota do Brasil por 7 a 1 contra a Alemanha na Copa do Mundo de 2014 é considerada uma das maiores polêmicas da história do futebol brasileiro?',
+    'A derrota por 7 a 1 nas semifinais da Copa de 2014 foi um choque global e considerada uma das maiores humilhações da história do futebol brasileiro. Foi cercada por questionamentos sobre o planejamento da equipe, a gestão do técnico Luiz Felipe Scolari e a pressão excessiva sobre os jogadores.'
+)
+
+criaCartao(
+    'Copa',
+    'Qual foi a grande polêmica que envolveu a seleção brasileira durante a Copa do Mundo de 1998, especialmente antes da final contra a França?',
+    'A polêmica envolveu o estado de saúde de Ronaldo, que sofreu uma convulsão horas antes da final. Ele foi inicialmente retirado da escalação, mas depois foi reintegrado ao time, o que gerou especulações sobre pressão de patrocinadores e a falta de clareza sobre sua condição.'
+)
+
